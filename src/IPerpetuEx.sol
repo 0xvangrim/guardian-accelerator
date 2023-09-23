b417378ba4bc96cec727d0c956675e42d6ce6ba1
Kenny-Gin1 <kenny.chung@live.se>
2023-09-23 12:44:08 +0200

    added interface with errors

---
+++ src/IPerpetuEx.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+interface IPerpetuEx {
+    /// Errors
+    error PerpetuEx__InvalidCollateral();
+    error PerpetuEx__NoOrder();
+    error PerpetuEx__InvalidSize();
+    error PerpetuEx__InvalidAmount();
+}
--- src/PerpetuEx.sol
+++ src/PerpetuEx.sol
@@ -2,12 +2,13 @@
 
 pragma solidity ^0.8.19;
 // import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Oracle} from "./Oracle.sol";
+import {convertFromUsdToBtc} from "./Oracle.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {IPerpetuEx} from "./IPerpetuEx.sol";
 
 contract PerpetuEx is ERC4626 {
     struct Order {
@@ -21,6 +22,7 @@ contract PerpetuEx is ERC4626 {
     using SafeERC20 for IERC20;
 
     AggregatorV3Interface public immutable s_priceFeed;
+
     IERC20 public immutable s_usdc;
 
     constructor(
@@ -31,6 +33,8 @@ contract PerpetuEx is ERC4626 {
         s_usdc = IERC20(_usdc);
     }
 
+    mapping(address => uint256) collateral;
+
     //  ====================================
     //  ==== External/Public Functions =====
     //  ====================================
@@ -39,9 +43,18 @@ contract PerpetuEx is ERC4626 {
 
     function withdraw(uint256 _amount) external {}
 
+    function depositCollateral(uint256 _amount) external {
+        if (_amount < 0)
+            revert PerpetuEx__InvalidAmount(
+                usdc.safeTransferFrom(msg.sender, address(this), _amount)
+            );
+    }
+
+    function withdrawCollateral(uint256 _amount) external {}
+
     function createOrder(uint256 _size, uint256 _collateral) external {
-        require(_size > 0, "Size must be greater than 0");
-        require(_collateral > 0, "Collateral must be greater than 0");
+        if (_size < 0) revert PerpetuEx__InvalidSize();
+        if (_collateral < 0) revert PerpetuEx__InvalidCollateral();
         //check that collateral is enough
     }
 
@@ -50,13 +63,14 @@ contract PerpetuEx is ERC4626 {
         uint256 _size,
         uint256 collateral
     ) external {
-        require(_size > 0, "Size must be greater than 0");
+        if (_size < 0) revert PerpetuEx__InvalidSize();
         //check that collateral is enough
     }
 
     function updateCollateral(uint256 _orderId, uint256 _collateral) external {
-        require(_collateral > 0, "Collateral must be greater than 0");
+        if (_collateral < 0) revert PerpetuEx__InvalidCollateral();
         //check that collateral is enough
+        usdc.safeTransferFrom(msg.sender, address(this), _collateral);
     }
 
     function updateOrder() external {}
@@ -69,7 +83,11 @@ contract PerpetuEx is ERC4626 {
     // ==== View Functions =====
     // =========================
 
-    function getPriceFeed() external view returns (AggregatorV3Interface) {
+    function getPriceFeed() view returns (AggregatorV3Interface) {
         return s_priceFeed;
+    }
+
+    function getConversionRate(uint256 _amount) view returns (uint256) {
+        return convertFromUsdToBtc(_amount);
     }
 }
